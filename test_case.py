15046fb1dd783fed73003dff35faf2debdad772d
Mohamed Eldesouki <mohamohamed@hbku.edu.qa>
2017-08-29 13:25:59 +0300

    test case

--- test_case.py
+++ test_case.py
@@ -4,4 +4,4 @@ from arabic_processing_cog.tokenization import ArabicTokenizer as at
 
 if __name__ == "__main__":
     for token in at.tokenize("بسم الله الرحن الرحيم"):
-        print token
+        print(token)
