fbbd87691523656d21609aec57d82dca506358ef
Mohamed Eldesouki <mohamohamed@hbku.edu.qa>
2017-08-29 13:07:14 +0300

    Creating the tokenization module

---
+++ arabic_processing_cog/tokenization.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+'''
+created on 2014 Mar 28
+by disooqi
+'''
+
+
+class Arabic_tokenizer:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def tokenize(text):
+        return text.strip().split()
+
+
+if __name__ == '__main__':
+    pass
