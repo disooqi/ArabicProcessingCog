8bc215c7c397f0099d49fa3d63ca365758d6892a
Mohamed Eldesouki <mohamohamed@hbku.edu.qa>
2017-08-29 13:24:08 +0300

    importing tokenization module and try it

--- test_case.py
+++ test_case.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
+from arabic_processing_cog.tokenization import ArabicTokenizer as at
+
+if __name__ == "__main__":
+    for token in at.tokenize("بسم الله الرحن الرحيم"):
+        print token
